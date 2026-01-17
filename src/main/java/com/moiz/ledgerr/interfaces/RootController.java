6fa864731578ef3cff60f940ada9546fc95f6a44
moiz-r <moizwwww@gmail.com>
2026-01-18 02:20:05 +0500

    renaming files

--- src/main/java/com/moiz/ledgerr/interfaces/RootController.java
+++ src/main/java/com/moiz/ledgerr/interfaces/RootController.java
@@ -1,4 +1,4 @@
-package com.moiz.ledgerr.controller;
+package com.moiz.ledgerr.interfaces;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
